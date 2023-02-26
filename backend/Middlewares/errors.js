bd358ad70fb2e849b754cb8a59e389a12b16c400
david medina <rafaelsanchez97.dm@gmail.com>
2023-02-25 22:03:30 -0300

    mas manejo de errores

--- backend/Middlewares/errors.js
+++ backend/Middlewares/errors.js
@@ -3,12 +3,34 @@ import ErrorHandler from '../utils/errorHandler.js';
 export let manejoErrores = (err, req, res, next) =>{
 
   err.statusCode = statusCode || 500;
-  err.message = err.message || 'error interno del servidor';
 
-  res.status(err.statusCode).json({
-    success: false,
-    error: err.stack
-  })
+  // manejo de errorrs de ambiente de desarrollo
+  if(process.env.NODE_ENV === "development"){
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message, //este creo que está mal
+      stack: err.stack
+    })
+  }
+
+  //production enviroment errors handlers
+  if(process.env.NODE_ENV === 'production'){
+    let error = {...err}
+
+    error.message = err.message
+
+    res.status(error.statusCode).json({
+      success: false,
+      error: error.message || "error interno del server"
+    })
+  }
+ // err.message = err.message || 'error interno del servidor';
+
+//  res.status(err.statusCode).json({
+//    success: false,
+//    error: err.stack
+//  })
 
 
 }
